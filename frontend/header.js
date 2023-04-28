0f56f5140ae261b0498452cddfb2b54287feee4f
Sam Zamany <hzamany2@gmail.com>
2023-04-28 18:22:30 -0400

    Create header.js

---
+++ frontend/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Your Name</h1>
+      <nav>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
