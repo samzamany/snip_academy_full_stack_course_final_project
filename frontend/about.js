dcf8ee3c36933b55f207d1ec2fbd2d087c8be121
Sam Zamany <hzamany2@gmail.com>
2023-04-28 18:23:05 -0400

    Create about.js

---
+++ frontend/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        Hello! I'm [Your Name], a full-stack web developer with a passion for
+        creating amazing user experiences.
+      </p>
+    </section>
+  );
+};
+
+export default About;
